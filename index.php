9c31d2464c18ed59fdc6546aace9aa641012a4c2
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-01-11 01:09:45 -0600

    estructura codigo
    estructura de directorios de codigo inicial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ienda online</title>
+	<link rel="stylesheet" type="text/css" href="css/movil.css" media="screen and (min-width:1px) and (max-with:800px)">
+	<link rel="stylesheet" type="text/css" href="css/pcmac.css" media="screen and (min-width:801px) and (max-with:4000px)">
+	<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+</head>
+<body>
+	<div id="contenedor">
+		<header>
+			<h1>Tienda online</h1>
+			<h2>Un negocio xxx</h2>
+		</header>
+		<section>
+		</section>
+		<footer>
+		</footer>
+	</div>
+</body>
+</html>
